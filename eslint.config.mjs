d2c6935a1fd3d6b2b540b5a497f48eeb59d35fd0
Elgin Ling Jun Hao <elginling@outlook.com>
2026-07-15 08:41:49 +0800

    Add ESLint config with security plugin, add eval test proof

---
+++ eslint.config.mjs
@@ -0,0 +1,58 @@
+import { defineConfig } from "eslint/config";
+import reactPlugin from 'eslint-plugin-react';
+import jestPlugin from 'eslint-plugin-jest';
+import testingLibraryPlugin from 'eslint-plugin-testing-library';
+import babelParser from '@babel/eslint-parser';
+import pluginSecurity from "eslint-plugin-security";
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,jsx}'],
+    languageOptions: {
+      parser: babelParser,
+      parserOptions: {
+        requireConfigFile: false,
+        babelOptions: {
+          presets: ['@babel/preset-react'],
+        },
+      },
+    },
+    plugins: {
+      react: reactPlugin,
+      security: pluginSecurity,
+    },
+    rules: {
+      ...reactPlugin.configs.recommended.rules,
+      'react/react-in-jsx-scope': 'off',
+      "security/detect-eval-with-expression": "error",
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+  {
+    files: ['**/*.test.jsx'],
+    plugins: {
+      jest: jestPlugin,
+      'testing-library': testingLibraryPlugin,
+    },
+    rules: {
+      ...jestPlugin.configs.recommended.rules,
+      ...testingLibraryPlugin.configs.react.rules,
+      'testing-library/await-async-events': 'off',
+    },
+    languageOptions: {
+      globals: {
+        test: true,
+        expect: true,
+        describe: true,
+        beforeEach: true,
+        afterEach: true,
+        it: true,
+        jest: true,
+      },
+    },
+  },
+]);
